a63338074712c3d3fdc26166154f1c7dba665e31
Aleksey <Alecsey913@mail.ru>
2024-02-27 23:32:37 +0300

    Add fields to tables

--- initdb.sql
+++ initdb.sql
@@ -1,22 +1,25 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    publications_count INTEGER NOT NULL
 );
 
-COPY users (id,name) FROM '/var/lib/db_data/users.csv' DELIMITER ',' CSV HEADER;
+COPY users (id,name,publications_count) FROM '/var/lib/db_data/users.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE publications (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    type INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    likes_count INTEGER DEFAULT 0,
     uid INTEGER REFERENCES users(id)
 );
 
-COPY publications (id,name,uid) FROM '/var/lib/db_data/publications.csv' DELIMITER ',' CSV HEADER;
+COPY publications (id,name,type,created_at,likes_count,uid) FROM '/var/lib/db_data/publications.csv' DELIMITER ',' CSV HEADER;
 
-CREATE TABLE user_publication (
-    user_id INTEGER REFERENCES users(id),
-    publication_id INTEGER REFERENCES publications(id),
-    PRIMARY KEY (user_id, publication_id)
+CREATE TABLE likes (
+    uid INTEGER REFERENCES users(id),
+    pid INTEGER REFERENCES publications(id),
+    PRIMARY KEY (pid, uid),
+    liked_at TIMESTAMP NOT NULL
 );
-
-INSERT INTO user_publication (user_id,publication_id) VALUES(1,1);
